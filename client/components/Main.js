d6c58cdc0abd40cbe27beaf4cd219841198d8db0
niawkung <niawkung2@gmail.com>
2017-11-17 14:00:58 +0700

    add react router

---
+++ client/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router';
+const Main = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>
+          <Link to="/">Reduxstagram</Link>
+        </h1>
+        {React.cloneElement(this.props.children)}
+      </div>
+    )
+  }
+});
+export default Main;
---
+++ client/components/Single.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const Single = React.createClass({
+  render() {
+    return (
+      <div className="single-photo">
+       I'm the Single Photo 
+      </div>
+    )
+  }
+});
+export default Single;

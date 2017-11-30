80f91b2d293ec312eb6823ee489df3d2d8772ef0
niaw <niawkung3@gmail.com>
2017-11-30 14:30:42 +0700

    :bug

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 import { render } from 'react-dom';
 
+// import css
 import css from './styles/style.styl';
 
+// import components
 import Main from './components/Main';
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
 
+// import react touter
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 const router = (

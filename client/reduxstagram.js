346dbf10f2fe241ea2fc2b71c3cdf5c243b4b22a
niawjunior <pasupol@dome.cloud>
2017-11-30 15:35:30 +0700

    :bug:

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -15,13 +15,15 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store, { history } from './store';
 const router = (
-  <Router history={browserHistory}>
-    <Route path="/" component={Main}>
-      <IndexRoute component={PhotoGrid}>
-      </IndexRoute>
-      <Route path="/view/:postId" component={Single}></Route>
-    </Route>
-  </Router>
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path="/" component={Main}>
+        <IndexRoute component={PhotoGrid}>
+        </IndexRoute>
+        <Route path="/view/:postId" component={Single}></Route>
+      </Route> 
+    </Router>
+  </Provider>
 )
 render(router,document.getElementById('root'));
 
